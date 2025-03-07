c735e148396ce435abb567e841aa18e3a8ee7fda
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-07 14:50:35 -0700

    Add button to resize grid

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const INITIAL_SIZE = 16;
+const INVALID_NUMBER_MSG =
+  "Not a valid number. Please enter an integer in the range 1 to 100.";
+
 makeGrid();
 document.addEventListener("mouseover", handleMouseOver);
+document.addEventListener("click", handleClick);
 
 function handleMouseOver(event) {
   if (event.target.className.includes("grid__row__square")) {
@@ -7,12 +12,39 @@ function handleMouseOver(event) {
   }
 }
 
-function makeGrid() {
-  for (let row = 0; row < 16; ++row) {
+function handleClick(event) {
+  if (event.target.className.includes("resize-button")) {
+    const newSize = getNewSize();
+    if (newSize != null) {
+      clearGrid();
+      makeGrid(newSize);
+    }
+  }
+}
+
+function getNewSize() {
+  let newSize;
+  let inputInvalid;
+
+  do {
+    newSize = prompt("Enter new size:");
+    if (newSize == null) return newSize; else newSize = Number(newSize);
+    inputInvalid = isNaN(newSize) || !Number.isInteger(newSize) ||
+      newSize < 1 || newSize > 100;
+    if (inputInvalid) alert(INVALID_NUMBER_MSG);
+  } while (inputInvalid);
+
+  return newSize;
+}
+
+function makeGrid(size = INITIAL_SIZE) {
+  const grid = document.querySelector(".grid");
+  
+  for (let row = 0; row < size; ++row) {
     const gridRow = document.createElement("div");
     gridRow.className = "grid__row";
 
-    for (let col = 0; col < 16; ++col) {
+    for (let col = 0; col < size; ++col) {
       const square = document.createElement("div");
       square.className = "grid__row__square";
       if (row == 0) square.className += " grid__row__square--top";
@@ -20,7 +52,13 @@ function makeGrid() {
       gridRow.appendChild(square);
     }
 
-    const grid = document.querySelector(".grid");
     grid.appendChild(gridRow);
+  }
+}
+
+function clearGrid() {
+  const grid = document.querySelector(".grid");
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
   }
 }
